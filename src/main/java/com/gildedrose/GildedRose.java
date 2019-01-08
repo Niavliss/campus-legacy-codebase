301ea5b9266b83637a4af00d8b7e5437d29d5f22
MoulinDigital <MoulinDigital@UC20361.ladrome.local>
2019-01-08 15:54:32 +0100

    refacto <50 condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,7 +7,6 @@ public class GildedRose {
         this.items = items;
     }
 
-
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
@@ -25,13 +24,7 @@ public class GildedRose {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
+                        if (items[i].sellIn < 11 || items[i].sellIn < 6 ) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
